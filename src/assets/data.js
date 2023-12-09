39abad5f8e080b68de075510681d4293927db334
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2023-12-09 15:55:02 +0530

    first commmit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question:"which device is reqired for the internet Connection",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+    {
+        question:"which continent has the hieghest number of Countries",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans:4,
+    },
+    {
+        question:"Junk E-mail is also Called?",
+        option1:"Spam",
+        option2:"Fake",
+        option3:"Archived",
+        option4:"Bin",
+        ans:1,
+    },
+    {
+        question:"A Compter cannot BOOT if it doesnot have the?",
+        option1:"Application Software",
+        option2:"Internet",
+        option3:"Operating System",
+        option4:"Mouse",
+        ans:3,
+    },
+    {
+        question:"First Page of website is termed as?",
+        option1:"Index Page",
+        option2:"Homepage",
+        option3:"sitemap",
+        option4:"Pen Drive",
+        ans:1,
+    },
+];
